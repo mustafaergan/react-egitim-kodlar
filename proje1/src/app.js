d249e941c87f811cad00c374cdbde969072b333c
Unsullied <unsullied@Unsullied-MacBook-Air.local>
2019-12-09 14:12:48 +0300

    yemek arası

--- proje1/src/app.js
+++ proje1/src/app.js
@@ -1,3 +1,40 @@
-var jsx = <h1>Merhaba Dünya</h1>
-var root = document.getElementById('app');
-ReactDOM.render(jsx, root);
+class Counter extends React.Component {
+
+    constructor () {
+        super()
+
+        this.onIncrease = this.onIncrease.bind(this)
+
+        this.state = {
+            count: 1
+        }
+    }
+
+    onIncrease () {
+
+        this.setState((prevState) => {
+            return {
+                count: prevState.count+1
+            }
+        }, () => {
+            
+            console.log('arttır!',this.state.count)
+        })
+
+    }
+
+    render () {
+
+        return (
+            <div>
+                <h1>Hello React</h1>
+                <p>Current Count: {this.state.count}</p>
+                <button onClick={this.onDecrease}>Azalt</button>
+                <button onClick={this.onIncrease}>Arttır</button>
+            </div>
+        )
+    }
+}
+
+var root = document.getElementById('app')
+ReactDOM.render(<Counter />, root)

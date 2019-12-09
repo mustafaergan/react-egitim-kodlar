b16cf101759d43adb0cf6520c970650361b2c177
Unsullied <unsullied@Unsullied-MacBook-Air.local>
2019-12-09 11:50:53 +0300

    first

---
+++ proje1/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var jsx = React.createElement("h1", null, "Merhaba D\xFCnya");
+var root = document.getElementById('app');
+ReactDOM.render(jsx, root);
---
+++ proje1/src/app.js
@@ -0,0 +1,3 @@
+var jsx = <h1>Merhaba Dünya</h1>
+var root = document.getElementById('app');
+ReactDOM.render(jsx, root);
